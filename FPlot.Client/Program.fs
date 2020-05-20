5a1a29b393d78d2a75650e132fc7c20a68a566a7
Installer <Installer@rluo-266.perpetuum.local>
2020-05-20 09:11:11 +0100

    Separated client and server

---
+++ FPlot.Client/Program.fs
@@ -0,0 +1,36 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+open System.Diagnostics
+
+[<EntryPoint>]
+let main argv =
+    let workDir = "FPlot.Server"
+    let binDir = "bin/Debug/netcoreapp2.2/"
+    let server = Path.Combine(binDir,"FPlot.Server.dll")
+
+    printfn "Running server from %s" (Path.GetFullPath(Path.Combine(workDir,server)))
+
+    let psi = ProcessStartInfo()    
+    psi.FileName <- "dotnet"
+    psi.Arguments <- server
+    psi.UseShellExecute <- false
+    psi.RedirectStandardOutput <- false
+    psi.RedirectStandardError <- false
+    psi.CreateNoWindow <- false
+    psi.WorkingDirectory <- workDir
+
+    let proc = new Process()
+    proc.StartInfo <- psi
+    let res = proc.Start()
+    printfn "Running (res = %A)" res
+    //printfn "%s" (proc.StandardOutput.ReadToEnd())
+
+    Console.WriteLine("Press anything to stop...")
+    Console.ReadKey() |> ignore
+
+    if proc.HasExited |> not then
+        proc.Kill()
+
+    0 // return an integer exit code
