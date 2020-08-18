f7a2b6c576b6e44889be743830b35acb1bf0c000
Matt Jones <matt.jones@perpetuum.com>
2020-08-18 19:50:19 +0100

    Fixed initial build output copying

--- build.bat
+++ build.bat
@@ -1,9 +1,43 @@
-dotnet build ./FizzPlot.Server/FizzPlot.Server.fsproj
-dotnet build ./FizzPlot/FizzPlot.fsproj
-dotnet build ./SchemaGen/SchemaGen.fsproj
-dotnet build ./Demo/Demo.fsproj
+echo off
 
-dotnet build -c Release ./FizzPlot.Server/FizzPlot.Server.fsproj
-dotnet build -c Release ./FizzPlot/FizzPlot.fsproj
-dotnet build -c Release ./SchemaGen/SchemaGen.fsproj
-dotnet build -c Release ./Demo/Demo.fsproj
+echo.
+echo ###
+echo ### FizzPlot.Server (Debug)
+echo ###
+dotnet build ./FizzPlot.Server/FizzPlot.Server.fsproj --nologo
+echo.
+echo ###
+echo ### FizzPlot (Debug)
+echo ###
+dotnet build ./FizzPlot/FizzPlot.fsproj --nologo
+echo.
+echo ###
+echo ### SchemGen (Debug)
+echo ###
+dotnet build ./SchemaGen/SchemaGen.fsproj --nologo
+echo.
+echo ###
+echo ### Demo (Debug)
+echo ###
+dotnet build ./Demo/Demo.fsproj --nologo
+
+echo.
+echo ###
+echo ### FizzPlot.Server (Release)
+echo ###
+dotnet build -c Release ./FizzPlot.Server/FizzPlot.Server.fsproj --nologo
+echo.
+echo ###
+echo ### FizzPlot (Release)
+echo ###
+dotnet build -c Release ./FizzPlot/FizzPlot.fsproj --nologo
+echo.
+echo ###
+echo ### SchemGen (Release)
+echo ###
+dotnet build -c Release ./SchemaGen/SchemaGen.fsproj --nologo
+echo.
+echo ###
+echo ### Demo (Release)
+echo ###
+dotnet build -c Release ./Demo/Demo.fsproj --nologo
